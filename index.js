fd1d9f51fcbd85064fe18af365836531ba0ca2a1
thyoon515 <thyoon515@gmail.com>
2022-04-24 23:25:25 -0400

    commit tree 5f61fa6f6a087500465b13fd28770d45a8d7d785

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    const p = document.querySelector('p')
+    p.textContent = "This is really cool!"
+    console.log("The DOM has loaded");
+
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
